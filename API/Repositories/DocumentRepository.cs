4b77b03bb79b0b76e26d1befada57c6458fe74cc
hukar <k.meshoub@gmail.com>
2025-03-25 11:46:19 +0100

    details poc

--- API/Repositories/DocumentRepository.cs
+++ API/Repositories/DocumentRepository.cs
@@ -2,11 +2,11 @@ namespace ScanFilePoc.Repositories;
 
 public class DocumentRepository
 {
-    private const string ConnectionString0 =
+    private const string ConnectionString =
         "Server=W2k22-Devel-T01; Database=FileScanDB; User ID=filescanuser; Password=filescan123.; Encrypt=False;";
 
     // Local CDE
-    private const string ConnectionString =
+    private const string ConnectionString0 =
         "Server=localhost; Database=ScanFileDB; User ID=sa; Password=huk@r2Xmen99; Encrypt=False;";
 
     // Local Maison
